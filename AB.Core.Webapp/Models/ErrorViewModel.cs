e6797fdb65bf0d9daad1709a2cc6b9d04aaa1065
shalesh <shalesh@bcms.com>
2021-07-18 14:03:44 +0530

    Initial commits

---
+++ AB.Core.Webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AB.Core.Webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
